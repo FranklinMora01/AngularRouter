7b851a129612f4d12bed42595ac0a6a77e4993cd
Franklin Mora <105868794+FranklinMora01@users.noreply.github.com>
2024-06-21 21:11:59 -0400

    se crearn loa pages y pagina para error 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { UsersComponent } from './pages/users/users.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'user/:id', component: UserComponent },
+    { path: 'users', component: UsersComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', component: Error404Component }
+];
